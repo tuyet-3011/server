ef9f3ef88c428a03d1a1755e91e125b67410a300
tuyet-3011 <tungtungmango4@gmail.com>
2026-06-17 10:29:34 +0700

    Fix variable declaration for responseHeaders

--- worker.js
+++ worker.js
@@ -95,7 +95,7 @@ addEventListener("fetch", async event => {
                 });
 
                 const response = await fetch(targetUrl, newRequest);
-                const responseHeaders = new Headers(response.headers);
+                let responseHeaders = new Headers(response.headers);
                 const exposedHeaders = [];
                 const allResponseHeaders = {};
                 for (const [key, value] of response.headers.entries()) {
